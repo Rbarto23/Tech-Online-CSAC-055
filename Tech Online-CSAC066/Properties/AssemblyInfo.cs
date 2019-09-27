a196aae698072711e29e6f61479438187c16443a
Rbarto Rbarto <c0739686@mylambton.ca>
2019-09-27 09:06:08 -0400

    First

--- Tech Online-CSAC066/Properties/AssemblyInfo.cs
+++ Tech Online-CSAC066/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tech_Online_CSAC055")]
+[assembly: AssemblyTitle("Tech_Online_CSAC066")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tech_Online_CSAC055")]
+[assembly: AssemblyProduct("Tech_Online_CSAC066")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("12f0ce0e-0496-48e4-8372-11581a072d88")]
+[assembly: Guid("387b8854-b221-4920-ab38-f956c80018b0")]
 
 // Version information for an assembly consists of the following four values:
 //
